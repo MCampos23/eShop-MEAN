160edec061011f45281836132b01c117b632d754
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-06-18 16:48:58 +0200

    updating products API

--- BACKEND/routes/products.js
+++ BACKEND/routes/products.js
@@ -3,9 +3,10 @@ const {Product} = require('../models/product')
 const express = require('express');
 const { Category } = require('../models/category');
 const router = express.Router();
+const mongoose = require('mongoose')
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find().select('name image -_id')
+    const productList = await Product.find().select('name image')
 
     if(!productList){
         res.status(500).json({succes:false})
@@ -14,8 +15,50 @@ router.get(`/`, async (req, res) => {
 })
 
 router.get(`/:id`, async (req, res) => {
-    const product = await Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id).populate('category')
 
+    if(!product){
+        res.status(500).json({success:false})
+    }
+    res.send(product)
+})
+
+router.delete(`/:id`, async (req, res) => {
+    const product = await Product.findByIdAndRemove(req.params.id)
+
+    if(!product){
+        res.status(500).json({success:false})
+    }
+    res.status(200).json({
+        success: true,
+        message: "The product is deleted"
+    })
+})
+
+router.put(`/:id`, async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id))
+        return res.status(400).send('Invalid Product ID')
+    const category = await Category.findById(req.body.category)
+    if (!category) return res.status(400).send('Invalid category')
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+        },
+        {new: true}
+        )
     if(!product){
         res.status(500).json({success:false})
     }
