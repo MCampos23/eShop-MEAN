8719dd616eb53eee05f2007c9386b8aab8292358
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-06-21 18:18:42 +0200

    hashing password

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -1,11 +1,37 @@
 
 const express = require('express');
 const router = express.Router();
+const {User} = require('../models/user')
+const mongoose = require('mongoose')
+var bcrypt =  require('bcryptjs');
 
 router.get(`/`, async (req, res) => {
-    
-    res.send('Users works!!')
+    const userList = await User.find()
+    res.send(userList)
 })
 
+router.post(`/`, async (req, res) => {
+     
+
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.passwordHash, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+
+    user = await user.save()
+
+    if(!user)
+       return res.status(500).send("The user cannot be created")
+    
+    res.send(user)    
+})
 
 module.exports = router
