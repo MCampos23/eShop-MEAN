4ed95e5d203c0bd99b9a90a63866a766e4f217d2
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-06-23 11:53:27 +0200

    Total e-shop sales and orderCount

--- BACKEND/routes/orders.js
+++ BACKEND/routes/orders.js
@@ -1,14 +1,18 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
 const {Order} = require('../models/order')
 const {OrderItem} = require('../models/order-item')
 
 
 router.get(`/`, async (req, res) => {
     const orderList = await Order.find()
-                                 .populate('user', 'name')
                                  .sort({'dateOrdered': -1})
+                                 .populate('user', 'name')
+                                 .populate({path: 'orderItems', populate:{
+                                                 path: 'product', populate: 'category'
+                                 }})
     if(!orderList){
         res.status(500).json({success: false})
     }
@@ -17,10 +21,10 @@ router.get(`/`, async (req, res) => {
 
 router.get(`/:id`, async (req, res) => {
     const order = await Order.findById(req.params.id)
-                             .populate('user', 'name')
-                             .populate({path: 'orderItems', populate:{
-                                             path: 'product', populate: 'category'
-                             }})
+                             
+                             .populate({path: 'orderItems', select: 'quantity product', populate:{path: 'product', select: 'price' }
+                              })
+                             
     if(!order){
         res.status(500).json({success: false})
     }
@@ -34,11 +38,21 @@ router.post(`/`, async (req, res) => {
             product: orderItem.product
         })
 
-        newOrderItem = await newOrderItem.save().populate('user', 'name')
+        newOrderItem = await newOrderItem.save()
 
         return newOrderItem._id
     }))
+
     const orderItemsIdsResolved = await orderItemsIds
+    const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId) => {
+        const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price')
+        const totalPrice = orderItem.product.price * orderItem.quantity
+        return totalPrice
+    }))
+
+    const totalPrice = totalPrices.reduce((a,b) => a + b, 0)
+
+    console.log(totalPrices)
     let order = new Order({
         orderItems: orderItemsIdsResolved,
         shippingAddress1: req.body.shippingAddress1,
@@ -49,7 +63,7 @@ router.post(`/`, async (req, res) => {
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
+        totalPrice: totalPrice,
         user: req.body.user
     })
 
@@ -61,4 +75,59 @@ router.post(`/`, async (req, res) => {
     res.send(order)
 })
 
+router.put(`/:id`, async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id))
+        return res.status(400).send('Invalid Product ID')
+
+   
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status,
+        },
+        {new: true}
+        )
+    if(!order){
+        res.status(500).json({success:false})
+    }
+    res.send(order)
+})
+
+router.delete('/:id', (req, res) => {
+    Order.findByIdAndRemove(req.params.id).then(async order => {
+        if(order){
+            await order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            })
+        return res.status(200).json({success: true, message: ' The order was deleted.'})
+        }else{
+            return res.status(404).json({success: false, message: 'Order not found'})
+        }
+    }).catch(err =>{
+        return res.status(400).json({success: false, error: err})
+    })
+})
+
+router.get('/get/totalsales', async(req, res) => {
+    const totalSales = await Order.aggregate([
+        { $group: {_id: null, totalSales: {$sum: '$totalPrice'}}}
+    ])
+
+    if(!totalSales) {
+        return res.status(400).send('The order cannot be generated')
+    }
+
+    res.send({ totalsales: totalSales.pop().totalSales})
+})
+
+router.get(`/get/count`, async (req, res) => {
+    const orderCount = await Order.countDocuments((count)=> count)
+
+    if(!orderCount){
+        res.status(500).json({succes:false})
+    }
+    res.send({
+        orderCount: orderCount,
+        })
+})
 module.exports = router
