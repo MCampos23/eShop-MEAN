88f4c0e28fbf4de8d2104fb29ced4576b279aa25
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-07-20 10:29:31 +0200

    protecting urls

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -52,28 +52,6 @@ router.put('/:id', async (req,res) => {
     
     res.send(updatedUser)
 })
-router.post(`/`, async (req, res) => {
-     
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.passwordHash, 10),
-        phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        street: req.body.street,
-        apartment: req.body.apartment,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country
-    })
-
-    user = await user.save()
-
-    if(!user)
-       return res.status(500).send("The user cannot be created")
-    
-    res.send(user)    
-})
 
 router.post(`/register`, async (req, res) => {
      
